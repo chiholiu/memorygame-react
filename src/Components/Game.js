7da2660100af09bdf516eb8f153665f1a18c6bbd
Chiho Liu <>
2019-04-15 12:54:02 +0200

    latest update

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,26 +1,70 @@
 import React, { Component } from 'react';
+// import { timingSafeEqual } from 'crypto';
 
 class Game extends Component {
     constructor(props) {
-        super(props);
+    super(props);
 
         this.state = {
-            memoryCards: ['mario', 'luigi', 'browser', 'peach', 'wario', 'toad', 'yoshi', 'bowser-junior', 'waluigi', 'diddy-kong-jr' ]
+            memoryCards: ['mario', 'luigi', 'browser', 'peach', 'wario', 'toad', 'yoshi', 'bowser-junior', 'waluigi', 'diddy-kong-jr' ],
+            cardArray: [],
+            newArray: [],
+            tempArray: [],
+            arrayHere: [],
+            count: 0
         }
     }
 
+    shuffleCard() {
+        this.setState({
+            cardArray: [...this.state.memoryCards, ...this.state.memoryCards]
+        }, () => {
+            this.setState({
+            newArray: this.state.cardArray.sort(() => 0.5 - Math.random())
+            })
+        })
+    }
+
+    checkCard(cardName, index) {
+        const item = {
+            name: cardName,
+            indexNumber: index,
+            flipped: true
+        };             
+    
+        this.setState({
+            tempArray: [...this.state.tempArray, item]
+        })
+
+        this.cardCounter(item);
+    }
+
+    cardCounter(item) {
+        this.setState({
+            count: this.state.count + 1
+        });
+
+        if(this.state.count > 0) {
+            this.nextRound(item); 
+        }
+    }
+
+    nextRound(getCard) {
+        this.setState({
+            tempArray: [...this.state.tempArray, getCard]
+        });
+        console.log(this.state.tempArray);
+    }
+
     render() {
-        let doubledMemoryCards = [...this.state.memoryCards, ...this.state.memoryCards];
-        let shuffledMemoryCards = doubledMemoryCards.sort(() => Math.random() - 0.5);
-       
+        // console.log(this.state.tempArray.length);
         return (
-            <div>
-                <ul>
-                   {shuffledMemoryCards.map((items, index) => (
-                       <li key={index}>{items}</li>
-                   ))}
-                </ul>
-            </div>
+            <ul>
+                <button onClick={() => this.shuffleCard()}>Click</button>
+                {this.state.cardArray.map((items, index) => (
+                    <li className="card" key={index} onClick={() => this.checkCard(items, index)}>{items}</li>
+                ))}
+            </ul>
         )
     }
 }
